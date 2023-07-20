4ea44dacb0ebb7bacdd120bc1dc0168910e0bea9
Max <maxshapira9@gmail.com>
2023-07-20 19:35:02 +0300

    register TodoItem models in django admin

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from tasks.models import TodoItem
+
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ("__str__",)
